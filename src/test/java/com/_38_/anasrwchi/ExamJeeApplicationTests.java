cc85dace2ae5119dba1a92da26249aa0f3175ed0
ANAS RWCHI <anassrwichi@gmail.com>
2026-05-11 14:33:22 +0100

    first commit

---
+++ src/test/java/com/_38_/anasrwchi/ExamJeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com._38_.anasrwchi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamJeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
